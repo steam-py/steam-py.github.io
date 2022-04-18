62e5b8be2f2bb157201734835efb82c1d584996d
Gobot1234 <50501825+Gobot1234@users.noreply.github.com>
2022-04-18 11:18:05 +0000

    update docs for Gobot1234/steam.py

--- docs/latest/_static/documentation_options.js
+++ docs/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.0a1+g694c805',
+    VERSION: '0.9.0a1+ge3b0a2f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'dirhtml',
